fbde08ebf659e11a4d8af9ada32a5e263158079e
Sebastian Willenbrink <sebastian.willenbrink@gmail.com>
2018-01-16 10:48:27 +0100

    Getting everything in place for dev...

--- compiler/CompilerTest.java
+++ compiler/CompilerTest.java
@@ -1,10 +1,200 @@
 package compiler;
+
 import static org.junit.Assert.assertEquals;
 
+import java.lang.Compiler;
 import org.junit.Test;
+import interpreter.Interpreter;
 
 public class CompilerTest {
 
+  @Test
+  public void testArraysBasic() {
+    String code = 
+        "int main() {\n" + 
+        "  int[] arr;\n" + 
+        "  int i, n, sum;\n" + 
+        "  n = 5;\n" + 
+        "  arr = new int[n];\n" + 
+        "  i = 0;\n" + 
+        "  while(i < n) {\n" + 
+        "    arr[i] = 2*i;\n" + 
+        "    i = i + 1;\n" + 
+        "  }\n" + 
+        "  sum = 0;\n" + 
+        "  i = 0;\n" + 
+        "  while(i < n) {\n" + 
+        "    sum = sum + arr[i];\n" + 
+        "    i = i + 1;\n" + 
+        "  }\n" + 
+        "  return sum;\n" + 
+        "}";
+    int[] assembly = java.lang.Compiler.compile(code);
+    int retVal = Interpreter.execute(assembly);
+    assertEquals(20, retVal);
+  }
+  
+  @Test
+  public void testArraysFunction() {
+    String code = 
+        "int sum(int[] arr, int n) {\n" + 
+        "  int i, sum;\n" + 
+        "  sum = 0;\n" + 
+        "  i = 0;\n" + 
+        "  while(i < n) {\n" + 
+        "    sum = sum + arr[i];\n" + 
+        "    i = i + 1;\n" + 
+        "  }\n" + 
+        "  return sum;\n" + 
+        "}\n" + 
+        "\n" + 
+        "int main() {\n" + 
+        "  int[] arr;\n" + 
+        "  int i, n, x, sum;\n" + 
+        "  n = 5;\n" + 
+        "  arr = new int[n];\n" + 
+        "  i = 0;\n" + 
+        "  while(i < n) {\n" + 
+        "    arr[i] = 2*i;\n" + 
+        "    i = i + 1;\n" + 
+        "  }\n" + 
+        "  return sum(arr, n);\n" + 
+        "}";
+    int[] assembly = java.lang.Compiler.compile(code);
+    int retVal = Interpreter.execute(assembly);
+    assertEquals(20, retVal);
+  }
+  
+  @Test
+  public void testArraysFunctionReturnsArray() {
+    String code = 
+        "int[] generateArray(int n) {\n" + 
+        "  int[] arr;\n" + 
+        "  int i;\n" + 
+        "  arr = new int[n];\n" + 
+        "  i = 0;\n" + 
+        "  while(i < n) {\n" + 
+        "    arr[i] = 2*i;\n" + 
+        "    i = i + 1;\n" + 
+        "  }\n" + 
+        "  return arr;\n" + 
+        "}\n" + 
+        "\n" + 
+        "int sum(int[] arr, int n) {\n" + 
+        "  int i, sum;\n" + 
+        "  sum = 0;\n" + 
+        "  i = 0;\n" + 
+        "  while(i < n) {\n" + 
+        "    sum = sum + arr[i];\n" + 
+        "    i = i + 1;\n" + 
+        "  }\n" + 
+        "  return sum;\n" + 
+        "}\n" + 
+        "\n" + 
+        "int main() {\n" + 
+        "  int[] arr;\n" + 
+        "  int n;\n" + 
+        "  n = 5;\n" + 
+        "  arr = generateArray(n);\n" + 
+        "  return sum(arr, n);\n" + 
+        "}";
+    int[] assembly = java.lang.Compiler.compile(code);
+    int retVal = Interpreter.execute(assembly);
+    assertEquals(20, retVal);
+  }
+  
+  @Test
+  public void testArrayLength() {
+    String code = 
+        "int[] generateArray() {\n" + 
+        "  int[] arr;\n" + 
+        "  int i;\n" + 
+        "  arr = new int[6];\n" + 
+        "  i = 0;\n" + 
+        "  while(i < length(arr)) {\n" + 
+        "    arr[i] = 2*i;\n" + 
+        "    i = i + 1;\n" + 
+        "  }\n" + 
+        "  return arr;\n" + 
+        "}\n" + 
+        "\n" + 
+        "int sum(int[] arr) {\n" + 
+        "  int i, sum;\n" + 
+        "  sum = 0;\n" + 
+        "  i = 0;\n" + 
+        "  while(i < length(arr)) {\n" + 
+        "    sum = sum + arr[i];\n" + 
+        "    i = i + 1;\n" + 
+        "  }\n" + 
+        "  return sum;\n" + 
+        "}\n" + 
+        "\n" + 
+        "int main() {\n" + 
+        "  int[] arr;\n" + 
+        "  arr = generateArray();\n" + 
+        "  return sum(arr);\n" + 
+        "}";
+    int[] assembly = java.lang.Compiler.compile(code);
+    int retVal = Interpreter.execute(assembly);
+    assertEquals(30, retVal);
+  }
+  
+  @Test
+  public void testPalindrome() {
+    String code = 
+        "int isPalindrome(int n) {\n" + 
+        "  int[] digits;\n" + 
+        "  int numberOfDigits, t, i, notMatching, digit;\n" + 
+        "\n" + 
+        "  numberOfDigits = 0;\n" + 
+        "  t = n;\n" + 
+        "  while (t != 0) {\n" + 
+        "    numberOfDigits = numberOfDigits + 1;\n" + 
+        "    t = t / 10;\n" + 
+        "  }\n" + 
+        "\n" + 
+        "  digits = new int[numberOfDigits];\n" + 
+        "\n" + 
+        "  i = 0;\n" + 
+        "  while (n != 0) {\n" + 
+        "    digit = n % 10;\n" + 
+        "    digits[i] = digit;\n" + 
+        "    n = n / 10;\n" + 
+        "    i = i + 1;\n" + 
+        "  }\n" + 
+        "\n" + 
+        "  notMatching = 0;\n" + 
+        "  i = 0;\n" + 
+        "  while (i < numberOfDigits / 2) {\n" + 
+        "    if (digits[i] != digits[numberOfDigits - i - 1])\n" + 
+        "      notMatching = notMatching + 1;\n" + 
+        "    i = i + 1;\n" + 
+        "  }\n" + 
+        "\n" + 
+        "  if (notMatching == 0)\n" + 
+        "    return 1;\n" + 
+        "  else\n" + 
+        "    return 0;\n" + 
+        "}\n" + 
+        " \n" + 
+        "int main() {\n" + 
+        "  int n;\n" + 
+        "  n = 0;\n" + 
+        "  n = n + isPalindrome(4224);\n" + 
+        "  n = n + isPalindrome(10);\n" + 
+        "  n = n + isPalindrome(99);\n" + 
+        "  n = n + isPalindrome(123321);\n" + 
+        "  n = n + isPalindrome(19910);\n" + 
+        "  n = n + isPalindrome(0990);\n" + 
+        "  n = n + isPalindrome(111111);\n" + 
+        "  n = n + isPalindrome(1112111);\n" + 
+        "  return n;" +
+        "}";
+    int[] assembly = java.lang.Compiler.compile(code);
+    int retVal = Interpreter.execute(assembly);
+    assertEquals(5, retVal);
+  }
+  
   @Test
   public void testGGT() {
     String ggtCode = "int ggt(int a, int b) {\n" + 
@@ -29,14 +219,14 @@ public class CompilerTest {
         "  r = ggt(a, b);\n" + 
         "  return r;\n" + 
         "}";
-    int[] assembly = Compiler.compile(ggtCode);
+    int[] assembly = java.lang.Compiler.compile(ggtCode);
     int retVal = Interpreter.execute(assembly);
     assertEquals(252, retVal);
   }
   
   @Test
   public void testFak() {
-    String fakCode = "int fak(int n) {\n" + 
+    String code = "int fak(int n) {\n" + 
         "  if(n == 0)\n" + 
         "    return 1;\n" + 
         "  return n*fak(n - 1);\n" + 
@@ -45,14 +235,14 @@ public class CompilerTest {
         "int main() {\n" + 
         "  return fak(6);\n" + 
         "}\n";
-    int[] assembly = Compiler.compile(fakCode);
+    int[] assembly = java.lang.Compiler.compile(code);
     int retVal = Interpreter.execute(assembly);
     assertEquals(720, retVal);
   }
   
   @Test
   public void testPrim() {
-    String primTestCode = "int prim(int n) {\n" + 
+    String code = "int prim(int n) {\n" + 
         "  int divisors, i;\n" + 
         "  divisors = 0;\n" + 
         "  \n" + 
@@ -84,7 +274,7 @@ public class CompilerTest {
         "  prims = prims + prim(5251);\n" + 
         "  return prims;\n" + 
         "}";
-    int[] assembly = Compiler.compile(primTestCode);
+    int[] assembly = java.lang.Compiler.compile(code);
     int retVal = Interpreter.execute(assembly);
     assertEquals(5, retVal);
   }
@@ -102,7 +292,7 @@ public class CompilerTest {
         "  r = ggt(a, b, a);\n" + 
         "  return r;\n" + 
         "}";
-    Compiler.compile(invalidCode);
+    java.lang.Compiler.compile(invalidCode);
   }
   
   @Test(expected = RuntimeException.class)
@@ -118,7 +308,7 @@ public class CompilerTest {
         "  r = ggt(a, b);\n" + 
         "  return r;\n" + 
         "}";
-    Compiler.compile(invalidCode);
+    java.lang.Compiler.compile(invalidCode);
   }
   
   @Test(expected = RuntimeException.class)
@@ -138,4 +328,3 @@ public class CompilerTest {
     Compiler.compile(invalidCode);
   }
 }
-//UTF-8 Encoded ä
--- compiler/FormatVisitor.java
+++ compiler/FormatVisitor.java
@@ -17,91 +17,114 @@ public class FormatVisitor implements Visitor
   @Override
   public void visit(Program item)
   {
-
+    for(Function func : item.getFunctions())
+      func.accept(this);
   }
 
   @Override
   public void visit(Function item)
   {
-
+    add(item.getType(), item.getName());
+    item.getParameters().accept(this);
+    for (int i = 0; i < item.getDeclarations().length; i++)
+    {
+      item.getDeclarations()[i].accept(this);
+    }
+    for (int i = 0; i < item.getStatements().length; i++)
+    {
+      item.getStatements()[i].accept(this);
+    }
   }
 
   @Override
   public void visit(Parameters item)
   {
-
+    for (int i = 0; i < item.getNames().length; i++)
+    {
+      add(item.getTypes()[i], item.getNames()[i], ",");
+    }
   }
 
   @Override
   public void visit(Declaration item)
   {
-
+    //TODO type
+    add("int ");
+    for(String name : item.getNames())
+      add(name, ",");
   }
 
   @Override
   public void visit(Assignment item)
   {
-
+    add(item.getName());
+    item.getExpression().accept(this);
   }
 
   @Override
   public void visit(Composite item)
   {
-
+    for(Statement stmt : item.getStatements())
+      stmt.accept(this);
   }
 
   @Override
   public void visit(IfThen item)
   {
-
+    //TODO
   }
 
   @Override
   public void visit(IfThenElse item)
   {
-
+    //TODO
   }
 
   @Override
   public void visit(While item)
   {
-
+    //TODO
   }
 
   @Override
   public void visit(Read item)
   {
-
+    add(item.getName(), "= read()");
   }
 
   @Override
   public void visit(Write item)
   {
-
+    add("= write (");
+    item.getExpression().accept(this);
+    add(");");
   }
 
   @Override
   public void visit(Return item)
   {
-
+    //TODO
   }
 
   @Override
   public void visit(Variable item)
   {
-
+    //TODO type missing?
+    add(item.getName());
   }
 
   @Override
   public void visit(Number item)
   {
-
+    add(item.getValue());
   }
 
   @Override
   public void visit(Binary item)
   {
-
+    item.getLhs().accept(this);
+    add(item.getOperator());
+    item.getRhs().accept(this);
   }
 
   @Override

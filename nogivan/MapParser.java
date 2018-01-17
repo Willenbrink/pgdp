df5a1f4cfe157ad18693e9105c6e02cb44353138
Sebastian Willenbrink <sebastian.willenbrink@gmail.com>
2018-01-17 10:28:33 +0100

    Blubb

---
+++ nogivan/MapParser.java
@@ -0,0 +1,15 @@
+package nogivan;
+
+import java.io.File;
+
+/**
+ * Diese Klasse erlaubt es, aus einer Datei im OSM-Format ein MapGraph-Objekt zu erzeugen. Sie nutzt
+ * dazu einen XML-Parser.
+ */
+public class MapParser {
+  public static MapGraph parseFile(String fileName) {
+    File inputFile = new File(fileName);
+
+    return null;
+  }
+}
